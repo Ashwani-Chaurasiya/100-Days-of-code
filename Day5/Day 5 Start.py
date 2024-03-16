6bf16f3143659bbaa3fcb650b7d884bf2b60ad2b
Ashwani-Chaurasiya <ashwanic661@gmail.com>
2024-03-16 14:37:06 +0530

    Day 1 to day 5

---
+++ Day5/Day 5 Start.py
@@ -0,0 +1,16 @@
+fruits = ['Apple', 'peach', 'pear']
+for fruit in fruits:
+  print(fruit)
+  print(fruit + "Pie")
+
+
+# For loop with Range:
+for number in range(1,11):
+  print(number)
+
+total = 0
+for number in range(1,101):
+  total += number
+print(total)
+
+
---
+++ Day5/un.py
@@ -0,0 +1,2 @@
+list = ["dsdfsdkfjsfs","sdfsddfs","dfs","ddf","sddf",234234]
+print(list)
